a227d37781a7dba7a0de9d59139a6190ef48937e
askerzade <pedro100500@gmail.com>
2019-10-17 14:33:57 +0500

    --second

--- src/main/java/mycodes/second.java
+++ src/main/java/mycodes/second.java
@@ -1,15 +1,14 @@
 package mycodes;
 
-
 import java.util.Random;
+
 public class second {
 
-
     public static void main(String[] args) {
-
+      int[] a = new int[100];
     Random random = new Random();
-        for (int i = 10; i < 20; i++) {
-            int rnd2 = random.nextInt(10)+11;
-            System.out.printf("Your random number is: %d\n", rnd2);
+        int rnd1 = random.nextInt();
+        System.out.println(rnd1);
+    }
 
-    }}}
+}
